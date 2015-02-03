0895527ba6f9c9a2cfc3aa57faef66c0e1ae97a0
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2015-02-03 21:23:16 +0200

    simplification
    Divert is significantly simplified. Multiple callbacks and sub-sync-ing
    are no longer supported.

--- test/divert-validations.js
+++ test/divert-validations.js
@@ -8,43 +8,4 @@ describe("divert parameters validation", function() {
          }, TypeError);
       done();
    });
-
-   it("divert ensures that last parameter is a sync callback", function(done) {
-      assert.throws(function() {
-            divert(function* () { return "I am a generator"; }, "but I am not a sync callback");
-         }, TypeError);
-      done();
-   });
-
-   it("several generators can be passed", function(done) {
-      assert.doesNotThrow(function() {
-            divert(function* () { return "I am a generator"; }, function* () { return "I am a generator too"; });
-            done();
-         });
-   });
-
-   it("several generators can be passed with sync parameter", function(done) {
-      divert(function* (sync) {
-         assert.doesNotThrow(function() {
-               divert(function* () { return "I am a generator"; }, function* () { return "I am a generator too"; }, sync);
-               done();
-            });
-      });
-   });
-
-   it("several generators can be passed as an array as well", function(done) {
-      assert.doesNotThrow(function() {
-            divert([function* () { return "I am a generator"; }, function* () { return "I am a generator too"; }]);
-            done();
-         });
-   });
-
-   it("several generators can be passed as an array plus sync parameter", function(done) {
-      divert(function* (sync) {
-         assert.doesNotThrow(function() {
-               divert([function* () { return "I am a generator"; }, function* () { return "I am a generator too"; }], sync);
-               done();
-            });
-      });
-   });
 });
